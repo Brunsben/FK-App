41d6c79c644fc6bc0fdd506f6e36390035761559
brunsben <brunsben@gmail.com>
2026-03-10 22:12:13 +0100

    chore: Boilerplate entfernen, error/not-found/loading Pages, Manifest harmonisieren, theme-init Fallback

--- public/theme-init.js
+++ public/theme-init.js
@@ -1,8 +1,9 @@
 // Dark-Mode sofort anwenden (vor React-Mount, verhindert Flicker)
 (function(){
   var c = (document.cookie.match(/(?:^|;\s*)fw_theme=(\w+)/) || [])[1];
-  if (c) {
-    localStorage.setItem('theme', c);
-    document.documentElement.classList.toggle('dark', c === 'dark');
+  var t = c || localStorage.getItem('theme');
+  if (t) {
+    document.documentElement.classList.toggle('dark', t === 'dark');
+    if (c) localStorage.setItem('theme', c);
   }
 })();
